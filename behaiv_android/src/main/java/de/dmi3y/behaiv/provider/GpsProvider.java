e915fe8e6c5adb0a3a3a6166f16011dc099a19d4
dmi3coder <dmi3coder@gmail.com>
2019-09-14 16:17:32 +0300

    Added GpsProvider

---
+++ behaiv_android/src/main/java/de/dmi3y/behaiv/provider/GpsProvider.java
@@ -0,0 +1,54 @@
+package de.dmi3y.behaiv.provider;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.core.SingleEmitter;
+import io.reactivex.rxjava3.core.SingleOnSubscribe;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static android.content.Context.LOCATION_SERVICE;
+
+public class GpsProvider implements Provider {
+
+    private Context context;
+    private final LocationManager locationManager;
+
+    public GpsProvider(Context context) {
+        locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
+        this.context = context;
+    }
+
+    @Override
+    public List<String> availableFeatures() {
+        return Arrays.asList("latitude", "longitude");
+    }
+
+    @Override
+    public Observable<List<Double>> subscribeFeatures() {
+        return null;
+    }
+
+    @Override
+    public Single<List<Double>> getFeature() {
+        return Single.create(new SingleOnSubscribe<List<Double>>() {
+            @Override
+            public void subscribe(final SingleEmitter<List<Double>> emitter) {
+                if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    //We use 0,0 coordinates if we don't have position as of now
+                    emitter.onSuccess(Arrays.asList(0.0, 0.0));
+                }
+                Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+                emitter.onSuccess(Arrays.asList(location.getLatitude(), location.getLongitude()));
+            }
+        });
+    }
+}
--- example_kotlin/src/main/java/de/dmi3y/behaiv/example/MainApplication.java
+++ example_kotlin/src/main/java/de/dmi3y/behaiv/example/MainApplication.java
@@ -1,11 +1,11 @@
 package de.dmi3y.behaiv.example;
 
-import android.app.Application;
 import android.support.multidex.MultiDexApplication;
 import android.util.Log;
 import de.dmi3y.behaiv.Behaiv;
 import de.dmi3y.behaiv.provider.AudioSettingsProvider;
 import de.dmi3y.behaiv.provider.DayTimeProvider;
+import de.dmi3y.behaiv.provider.GpsProvider;
 
 public class MainApplication extends MultiDexApplication {
     private Behaiv behaiv;
@@ -14,6 +14,7 @@ public class MainApplication extends MultiDexApplication {
     public void onCreate() {
         super.onCreate();
         behaiv = Behaiv.with("chats")
+                .setProvider(new GpsProvider(this))
                 .setProvider(new DayTimeProvider())
                 .setProvider(
                         new AudioSettingsProvider.Builder(this).checkHeadsetConnected().build()
